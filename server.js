10e805f1720c4fb2c0ef4e23172b02e356360441
unknown <alexm1397@gmail.com>
2021-11-22 15:04:21 -0600

    added homepage routes and rendering

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 
 const express = require('express');
-const routes = require('./routes');
+const path = require('path');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
+//handlebar.js:
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//tells express to use handlebars?
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // this is middleware - tells server that incoming data is a JSON Object
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 'starts' routes essentially
 // in user-route.js required {User} is run to create User TABLE in mysql db
