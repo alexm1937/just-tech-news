c12563ab4ba70c254d9b168e957a1f60dfa42b5c
unknown <alexm1397@gmail.com>
2021-11-26 14:47:36 -0600

    added partial sheets and plural handlers (helpers)

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
+
 
 const sess = {
     secret: 'Super secret secret',
